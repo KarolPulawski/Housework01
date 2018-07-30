05e3a23de74ce6327298ad28d8698d6f525ac616
KarolPulawski <karol.j.pulawski@gmail.com>
2018-07-30 23:08:05 +0200

    Module 3 / Day 1 / Housework / Exercise 2.

--- src/pl/coderslab/Main2.java
+++ src/pl/coderslab/Main2.java
@@ -1,4 +1,9 @@
 package pl.coderslab;
 
 public class Main2 {
+
+    String reg1= "^(\\d*)(\\.)?(\\d*)$";
+    String reg2= "^(\\d*)(\\.)?(\\d*)[eE]-(\\d+)$";
+    String reg3= "^[0-3][0-9]\\/[0-1][0-9]\\/[0-9]{1,4}$";
+    String reg4= "^[2-9][0-9]{3}\\-[0-1][0-9]\\-[0-3][0-9]$";
 }
